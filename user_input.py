285eae404f48fe22d57834c8e64de4f312d891ea
Taiwo Sodiq <oyeniyi4real@gmail.com>
2024-03-19 04:42:54 +0100

    corrected the spacing

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Your name")
+age = input("how old are you")
+location = input("where are you now")
+print("Hello " + name + ", you are "+ age +" years old and live in " +location )
+
+
